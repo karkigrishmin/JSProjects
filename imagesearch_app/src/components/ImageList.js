063471e14a4ea14a6ad06b7f35d8176166bc26a0
Grishmin karki <grishmink@gmail.com>
2020-10-05 16:20:15 +0545

    rendered imagelist

--- imagesearch_app/src/components/ImageList.js
+++ imagesearch_app/src/components/ImageList.js
@@ -1,13 +1,21 @@
 import React from "react";
 
-const ImageList = (images) => {
+const ImageList = ({ images }) => {
   console.log(images);
   //   if (images.length !== 0) {
   //     const imageList = images.map((image) => {
   //       return <img src={image.urls.regular} alt="img" />;
   //     });
   //   }
-  const image = <div>imagelist</div>;
+  const image = images.map((image) => {
+    return (
+      <img
+        src={image.urls.regular}
+        key={image.id}
+        alt={image.alt_description}
+      />
+    );
+  });
 
   return <div>{image}</div>;
 };
