6e5a9cb0f692e7e86557aeaf6ec217aad3bd757f
Grishmin karki <grishmink@gmail.com>
2020-10-25 13:32:41 +0545

    added total amount

--- expense_tracker/src/components/Balance.js
+++ expense_tracker/src/components/Balance.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 export function Balance() {
+  const { transactions } = useContext(GlobalContext);
+
+  const amounts = transactions.map((transaction) => transaction.amount);
+
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+  console.log(total);
+
   return (
     <div className="text">
       <h6>Your Balance</h6>
       <div className="amt">
-        <p>Rs500</p>
+        <p>Rs{total}</p>
       </div>
     </div>
   );
--- expense_tracker/src/components/TransactionList.js
+++ expense_tracker/src/components/TransactionList.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { GlobalContext } from "../context/GlobalContext";
+import { GlobalContext } from "../context/GlobalState";
 import { Transaction } from "./Transaction";
 
 export function TransactionList() {
