28c594995eb3771beaa63fd493649c2fbf111a8e
Grishmin karki <grishmink@gmail.com>
2020-10-25 14:53:29 +0545

    total income and expense

--- expense_tracker/src/components/IncomeExpense.js
+++ expense_tracker/src/components/IncomeExpense.js
@@ -1,18 +1,32 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 export function IncomeExpense() {
+  const { transactions } = useContext(GlobalContext);
+
+  const amounts = transactions.map((transaction) => transaction.amount);
+
+  const income = amounts
+    .filter((amt) => amt > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
+  const expense =
+    amounts.filter((amt) => amt < 0).reduce((acc, item) => (acc += item), 0) *
+    -(1).toFixed(2);
+
   return (
     <div className="income-expense">
       <div className="income">
         <h6>Income</h6>
         <div className="amt">
-          <p>Rs10000</p>
+          <p>Rs{income}</p>
         </div>
       </div>
       <div className="expense">
         <h6>Expense</h6>
         <div className="amt">
-          <p>Rs500</p>
+          <p>Rs{expense}</p>
         </div>
       </div>
     </div>
