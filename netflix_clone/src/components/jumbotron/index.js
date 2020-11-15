9ba1f64ebf6ecfc2ae5f5db7a4b65dffe3bee9cc
Grishmin karki <grishmink@gmail.com>
2020-11-15 20:36:59 +0545

    jumbotron almost completed

--- netflix_clone/src/components/jumbotron/index.js
+++ netflix_clone/src/components/jumbotron/index.js
@@ -6,6 +6,7 @@ import {
   Title,
   SubTitle,
   Image,
+  Item,
 } from "./styles/jumbotron";
 
 export default function Jumbotron({
@@ -13,7 +14,11 @@ export default function Jumbotron({
   direction = "row",
   ...restProps
 }) {
-  return <Inner direction={direction}>{children}</Inner>;
+  return (
+    <Item {...restProps}>
+      <Inner direction={direction}>{children}</Inner>
+    </Item>
+  );
 }
 
 Jumbotron.Container = function JumbotronContainer({ children, ...restProps }) {
