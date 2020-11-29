083b8a13d80f5078e11f43d38a5032d739b5177d
Grishmin karki <grishmink@gmail.com>
2020-11-29 19:20:14 +0545

    accordion component

---
+++ netflix_clone/src/components/accordion/styles/accordion.js
@@ -0,0 +1,16 @@
+import styled from "styled-components/macro";
+
+export const Container = styled.div`
+  display: flex;
+  border-bottom: 8px solid #222;
+`;
+
+export const Inner = styled.div``;
+
+export const Title = styled.h1``;
+
+export const Item = styled.div``;
+
+export const Header = styled.div``;
+
+export const Body = styled.div``;
